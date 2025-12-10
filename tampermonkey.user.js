7b73b91cc9bd3786a894e9f25b262b1756413847
FireT <firetofficial@gmail.com>
2025-12-10 18:34:38 +0700

    Upload Rophim-No-Ads

---
+++ tampermonkey.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Rophim VIP - Block All Ads
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Bypass VIP - No ads
+// @author       FireT
+// @match        *://www.rophim.me/*
+// @match        *://www.rophim.mx/*
+// @match        *://www.rophim.li/*
+// @match        https://goatembed.com/*
+// @require      https://raw.githubusercontent.com/firetofficial/Rophim-No-Ads/refs/heads/main/rophim_noads.js
+// @homepageURL   https://github.com/firetofficial/Rophim-No-Ads
+// @downloadURL   https://raw.githubusercontent.com/firetofficial/Rophim-No-Ads/refs/heads/main/tampermonkey.user.js
+// @updateURL     https://raw.githubusercontent.com/firetofficial/Rophim-No-Ads/refs/heads/main/tampermonkey.user.js
+// @run-at       document-start
+// @grant        none
+// @icon         https://avatars.githubusercontent.com/u/120646974?s=48&v=4
+// ==/UserScript==
